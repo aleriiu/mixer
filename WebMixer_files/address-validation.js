c036e3d8efdefe0c7f166b3b8ce2f9b5639fc449
SHVEPS <shveps@MacBook-Pro-SHVEPS.local>
2023-09-13 11:24:48 +0300

    small changes

--- WebMixer_files/address-validation.js
+++ WebMixer_files/address-validation.js
@@ -1,3 +1,11 @@
-var addressInputs=[...document.querySelectorAll('.mix__bar__pages .inputs__wrap .input__wrap input:not(.amount__selector)')];var addressInputForms=[...document.querySelectorAll('.mix__bar__pages .inputs__wrap')];var mixSpecs=[...document.querySelectorAll('.mix__bar__pages .mix__specs')];addressInputs.forEach(element=>{element.addEventListener('input',function(){let input=this.value.trim();if(input.length>=26){if(this.nextElementSibling!=null){this.nextElementSibling.disabled=false;}else if(this.parentElement.nextElementSibling.tagName=='BUTTON'){this.parentElement.nextElementSibling.disabled=false;}
-this.style.border='none';}else{this.style.border='2px solid red';if(this.nextElementSibling!=null){this.nextElementSibling.disabled=true;}
-else if(this.parentElement.nextElementSibling.tagName=='BUTTON'){this.parentElement.nextElementSibling.disabled=true;}}});});
+var addressInputs = [...document.querySelectorAll('.mix__bar__pages .inputs__wrap .input__wrap input:not(.amount__selector)')]; var addressInputForms = [...document.querySelectorAll('.mix__bar__pages .inputs__wrap')]; var mixSpecs = [...document.querySelectorAll('.mix__bar__pages .mix__specs')]; addressInputs.forEach(element => {
+    element.addEventListener('input', function () {
+        let input = this.value.trim(); if (input.length >= 26) {
+            if (this.nextElementSibling != null) { this.nextElementSibling.disabled = false; } else if (this.parentElement.nextElementSibling.tagName == 'BUTTON') { this.parentElement.nextElementSibling.disabled = false; }
+            this.style.border = 'none';
+        } else {
+            this.style.border = '2px solid red'; if (this.nextElementSibling != null) { this.nextElementSibling.disabled = true; }
+            else if (this.parentElement.nextElementSibling.tagName == 'BUTTON') { this.parentElement.nextElementSibling.disabled = true; }
+        }
+    });
+});
